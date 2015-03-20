264ad83dc75e8c92768d611e526a83394dc87bf0
Abdelkarim Erradi <erradi@live.com>
2015-03-20 19:30:22 +0300

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,54 @@
+[
+  {
+    "id": 1,
+    "firstName": "Juha",
+    "lastName": "Dahak",
+    "phone": "8888-9999",
+    "mobile": "7777-888",
+    "email": "juha@test.com",
+    "address": {
+      "street": "5 Test St",
+      "city": "Doha",
+      "country": "Qatar"
+    }
+  },
+  {
+    "id": 2,
+    "firstName": "Abbas",
+    "lastName": "Ibn Farnas",
+    "phone": "6666-9999",
+    "mobile": "9999-888",
+    "email": "abbas@test.com",
+    "address": {
+      "street": "50 Test St",
+      "city": "Doha",
+      "country": "Qatar"
+    }
+  },
+  {
+    "id": 3,
+    "firstName": "Samir",
+    "lastName": "Saghir",
+    "phone": "7777-9999",
+    "mobile": "9999-888",
+    "email": "samir@test.com",
+    "address": {
+      "street": "22 Aspire St",
+      "city": "Dubai",
+      "country": "UAE"
+    }
+  },
+  {
+    "id": 4,
+    "firstName": "Jarir",
+    "lastName": "Bookstore",
+    "phone": "3333-9999",
+    "mobile": "2222-888",
+    "email": "jarir@test.com",
+    "address": {
+      "street": "33 Salman St",
+      "city": "Jeddah",
+      "country": "Saudi"
+    }
+  }
+]
